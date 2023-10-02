0b3aa42e0ed52a978487b5624c56f896455a7890
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-02 14:08:56 +0400

    Change example status to out of maintenance

--- CS/Example/Models/ReportDesignerModel.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using DevExpress.DataAccess.Sql;
-
-namespace Example.Models {
-    public class ReportDesignerModel {
-        public Dictionary<string, object> DataSources { get; set; }
-    }
-}
